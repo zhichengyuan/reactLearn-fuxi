0f0bc8c882660b6921dc25531427c9aec9c2346b
yuanzhicheng <1798788388@qq.com>
2020-12-07 14:00:57 +0800

    yzc

--- react-learn/src/App.js
+++ react-learn/src/App.js
@@ -1,13 +1,34 @@
 
+import src1 from './assets/1.jpg'
+import src2 from './assets/2.jpg'
+import src3 from './assets/3.jpg'
+
+const srcArr = [src1,src2,src3];
+
+let index = 0;//显示的图片索引
+
+function render() {
+  return (
+      <img src={srcArr[index]} alt=""/>
+  );
+}
+
+function start() {
+  setInterval(() => {
+    console.log(index);
+    index = (index + 1 ) % 3;
+    render();
+  }, 2000);
+}
+
 function App() {
-  const h1 = <h1>hello,world</h1>
-  console.log(h1);
   return (
     <div className="App">
-      
-      {h1}
+      {render()}
     </div>
   );
 }
+
+
 
 export default App;
--- react-learn/src/index.js
+++ react-learn/src/index.js
@@ -2,8 +2,43 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-ReactDOM.render(
-    <App />,
-  document.getElementById('root')
-);
+import src1 from './assets/1.jpg'
+import src2 from './assets/2.jpg'
+import src3 from './assets/3.jpg'
+import './index.css'
+
+const srcArr = [src1,src2,src3];
+
+let index = 0;//显示的图片索引
+
+let timer = null;
+
+const container = document.getElementById('root');
+
+function render() {
+  ReactDOM.render(<img src={srcArr[index]} alt=""/>,container)
+}
+
+function start() {
+  stop();
+  timer = setInterval(() => {
+    index = (index + 1 ) % 3;
+    render();
+  }, 2000);
+}
+
+// 停止计时器
+function stop() {
+  clearInterval(timer);
+}
+
+container.onmouseenter = function () {
+  stop();
+}
+
+container.onmouseleave = function(){
+  start();
+}
+render();
+start();
 
