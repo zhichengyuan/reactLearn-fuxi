261f357e223153c93669b677653244da96c75b3c
yuanzhicheng <1798788388@qq.com>
2021-01-06 17:08:03 +0800

    yzc

--- react-learn/src/App.js
+++ react-learn/src/App.js
@@ -1,30 +1,13 @@
 import React, { Component } from 'react'
-import TreeLayout from './components/common/ThreeLayout'
+// import Numberinput from './components/Numberinput'
+import FormTest from './components/FormTest'
 export default class App extends Component {
- 
+  
   render() {
     
     return (
       <div>
-        <TreeLayout
-          gap={20}
-          left={<div style={{
-            border:'2px solid #008c8c'
-          }}>左边栏</div>}
-          right={
-            <div style={{
-              border:'1px solid red'
-            }}>
-              右边栏
-            </div>
-          }
-        >
-          <div style={{
-            border:'2px solid red'
-          }}>
-            主区域
-          </div>
-        </TreeLayout>
+        <FormTest/>
       </div>
       
     )
