ef152d93d613034bb28e0f91f1dd4c98de0c6189
yuanzhicheng <1798788388@qq.com>
2021-01-08 11:45:38 +0800

    zyc

--- react-learn/src/ValidationComp.js
+++ react-learn/src/ValidationComp.js
@@ -12,11 +12,13 @@ class ValidationComp extends Component {
     static propTypes = {
         a:PropTypes.number.isRequired,//必须是数字并且必填
         b:PropTypes.bool.isRequired,//必须是bool并且必填
+        onClick:PropTypes.func,//必须是函数
+        c:PropTypes.any,//1.可以设置必填 2.队列保持整齐（所有属性都在该对象中）
     }
     render() {
         return (
             <div>
-                {this.props.a}
+                {this.props.a} 
             </div>
         );
     }
