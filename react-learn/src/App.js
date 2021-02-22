118906eda9437b94290c4870ab8a27cfdd3157f5
yuanzhicheng <1798788388@qq.com>
2021-02-22 10:31:42 +0800

    yzc

--- react-learn/src/App.js
+++ react-learn/src/App.js
@@ -1,11 +1,28 @@
 import React, { Component } from 'react'
-import Test from './components/Test'
+import ReactDOM from 'react-dom'
+
+function ChildA(){
+    return ReactDOM.createPortal(<div className="child-a" style={{
+        marginTop:200
+    }}>
+        <h1>1</h1>
+    <ChildB/>
+</div>,document.querySelector('.modal'))
+}
+function ChildB() {
+    return <div className="child-b">
+        <h1>2</h1>
+    </div>
+}
 
 export default class App extends Component {
     render() {
         return (
-            <div>
-                <Test/>
+            <div className="app" onClick={(e) => {
+                console.log('APP被点击了',e.target)
+            }}>
+                <h1>APP</h1>
+                <ChildA/>
             </div>
         )
     }
