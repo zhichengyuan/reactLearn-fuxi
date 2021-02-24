22cc02e0508150b108e91aa22e144914af7264b8
yuanzhicheng <1798788388@qq.com>
2021-02-24 11:14:37 +0800

    yzc

--- react-learn/src/App.js
+++ react-learn/src/App.js
@@ -1,25 +1,57 @@
 import React, { Component } from 'react'
-var prev
+import {getAllStudents} from './services/student'
+
+class StudentList extends Component {
+    render() {
+        const stus = this.props.stuList.map(it => <Student key={it.id} name={it.name} age={it.age} address={it.address}/>)
+        return (
+            <ul>
+                {stus}
+            </ul>
+        );
+    }
+}
+
+class Student extends Component {
+    render() {
+        return <li>
+                <span>{this.props.name}</span>
+                <span>{this.props.age}</span>
+                <span>{this.props.address}</span>
+            </li>
+    }
+}
+
 export default class App extends Component {
+
+    state = {
+        stuList:[]
+    }
+
+    loadStudents = async () => {
+        const stus = await getAllStudents();
+        this.setState({
+            stuList:stus
+        })
+    }
+
     render() {
         return (
-            <div onClick={(e) => {
-                console.log(prev === e);
-                console.log('react div被点击了')
-            }}>
-                <button onClick={(e) => {
-                    console.log("react:按钮被点击了");
-                    prev = e;
-                    // e.persist();
-                    setTimeout(() => {
-                        console.log(e.type);
-                    },0)
-                    // console.log(e.nativeEvent)
-                    // e.nativeEvent.stopImmediatePropagation()
-                    // console.log(e.isPropagationStopped());
-                    // e.stopPropagation();
-                    // console.log(e.isPropagationStopped());
-                }}>按钮</button>
+            <div>
+                {/* 显示学生集合 */}
+                <button onClick={this.loadStudents}>加载学生数据</button>
+                <button onClick={() => {
+                    this.setState({
+                        stuList:[]
+                    })
+                }}>清空数据</button>
+                <button onClick={() => {
+                    this.setState({
+                        stuList:this.state.stuList.sort(() => Math.random() - 0.5)
+                    })
+                }}>打乱顺序</button>
+
+                <StudentList stuList={this.state.stuList}/>
             </div>
         )
     }
