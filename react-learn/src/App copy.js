284e9f13158cd619fa9ef1131109c9b53d8b2a01
yuanzhicheng <1798788388@qq.com>
2021-03-04 17:56:23 +0800

    yzc

--- react-learn/src/App copy.js
+++ react-learn/src/App copy.js
@@ -1,9 +1,8 @@
 // import React,{useReducer} from  'react'
-import React,{useState,useCallback} from  'react'
+import React,{useState} from  'react'
 
 class Test extends React.Component {
     render () {
-        console.log('Test渲染了')
         return <div>
             <h1>{this.props.text}</h1>
             <button onClick={() => {
@@ -14,25 +13,17 @@ class Test extends React.Component {
     }
 }
 
-function Parent(){
-    const [txt, setTxt] = useState(1);
-    const [n, setN] = useState(0);
-    const handleClick = useCallback(() => {
-       
-        setTxt(Math.random());
-    },[])
+export default function App() {
+    const [txt, setTxt] = useState(1)
+    const [n, setN] = useState(0)
     return <div>
         {/* 函数的地址每次渲染都发生了变化，导致了子组件跟着重新渲染，若子组件是经过优化的组件，则可能导致优化失效 */}
-       <Test text={txt} onClick={handleClick}/>
+       <Test text={txt} onClick={(value) => {
+           setTxt(value);
+       }}/>
        <input type="number" value={n} onChange={(e) => {
            setN(e.target.value)
        }}/>
-    </div>
-}
-
-export default function App() {
-    return <div>
-       <Parent/>
     </div>
 }
 
