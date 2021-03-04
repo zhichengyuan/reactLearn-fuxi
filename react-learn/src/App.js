d28dba6a59dbe99553457003090c0602c0eeda31
yuanzhicheng <1798788388@qq.com>
2021-03-04 18:24:22 +0800

    yzc

--- react-learn/src/App.js
+++ react-learn/src/App.js
@@ -1,38 +1,29 @@
 // import React,{useReducer} from  'react'
-import React,{useState,useCallback} from  'react'
+import React,{useState,useMemo} from  'react'
 
-class Test extends React.Component {
-    render () {
-        console.log('Test渲染了')
-        return <div>
-            <h1>{this.props.text}</h1>
-            <button onClick={() => {
-                let newValue = this.props.text + 1;
-                this.props.onClick && this.props.onClick(newValue)
-            }}>+1</button>
-        </div>
-    }
-}
-
-function Parent(){
-    const [txt, setTxt] = useState(1);
-    const [n, setN] = useState(0);
-    const handleClick = useCallback(() => {
-       
-        setTxt(Math.random());
-    },[])
-    return <div>
-        {/* 函数的地址每次渲染都发生了变化，导致了子组件跟着重新渲染，若子组件是经过优化的组件，则可能导致优化失效 */}
-       <Test text={txt} onClick={handleClick}/>
-       <input type="number" value={n} onChange={(e) => {
-           setN(e.target.value)
-       }}/>
-    </div>
+function Item(props) {
+    return <li>{props.value}</li>
 }
 
 export default function App() {
+    const [range, ] = useState({min:1,max:10000});
+    const [n, setN] = useState(0)
+    const list = useMemo(() => {
+        const list = []
+        for(let i = range.min;i<=range.max;i++) {
+            console.log(i);
+            list.push(<Item key={i} value={i}/>)
+        }
+        return list
+    },[range.min,range.max])
+    
     return <div>
-       <Parent/>
+        <ul>
+            {list}
+        </ul>
+        <input type="number" value={n} onChange={e=>{
+            setN(parseInt(e.target.value))
+        }}/>
     </div>
 }
 
