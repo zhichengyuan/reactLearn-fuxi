7625ba275b94b388f2ca7077313f42c79661dbf2
yuanzhicheng <1798788388@qq.com>
2021-03-08 15:06:47 +0800

    yzc

--- react-learn/src/App.js
+++ react-learn/src/App.js
@@ -2,19 +2,28 @@
 import React,{useState} from  'react'
 import { CSSTransition } from 'react-transition-group';
 import './App.css'
+import "animate.css"
 
 function MyTransition({show,children}) {
-    return <CSSTransition mountOnEnter appear timeout={2000} in={show}>
+    return <CSSTransition mountOnEnter appear timeout={800} in={show}
+            classNames={{
+                exitActive:"animate__fadeOutLeft",
+                exitDone:"exit-done",
+                enterActive:"animate__fadeInRight",
+                appearActive:"animate__fadeInRight"
+                // enterActive:"animate__bounceIn"
+            }}
+            >
                 {children}
             </CSSTransition>
 }
 
 function Comp1() {
-    return <h1 className="title">组件1</h1>
+    return <h1 className="title animate__animated animate__faster">组件1</h1>
 }
 function Comp2() {
     // return <h1>组件2</h1>
-    return  <h1 className="title">组件2</h1>
+    return  <h1 className="title animate__animated animate__faster">组件2</h1>
            
 }
 
@@ -27,7 +36,7 @@ export default function App() {
               <MyTransition show={show}>
                     <Comp1 ></Comp1>
               </MyTransition>
-              <MyTransition show={show}>
+              <MyTransition show={!show}>
                     <Comp2 ></Comp2>
               </MyTransition>
             
