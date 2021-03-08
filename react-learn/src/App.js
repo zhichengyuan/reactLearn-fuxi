cc9e4d764be9e934cdf7ab0f286c1cbc5a59287e
yuanzhicheng <1798788388@qq.com>
2021-03-08 17:00:53 +0800

    yzc

--- react-learn/src/App.js
+++ react-learn/src/App.js
@@ -1,27 +1,39 @@
 // import React,{useReducer} from  'react'
 import React,{useState} from  'react'
-import { CSSTransition,SwitchTransition } from 'react-transition-group';
-import './App.css'
+import { CSSTransition,TransitionGroup } from 'react-transition-group';
 import "animate.css"
+import './App.css'
 
 
 export default function App() {
-    const [show, setShow] = useState(true)
+    const [show, setShow] = useState(true);
+    const [tasksList,setTasksList] = useState([
+        {id:2,name:'任务1'},
+        {id:3,name:'任务2'},
+    ])
     return (
-      <div className="container">
-          <SwitchTransition mode="out-in">
-              <CSSTransition appear timeout={1000} key={show}
-                classNames={{
-                    exitActive:"animate__bounceOut",
-                    enterActive:"animate__bounceIn"
-                }}
-              >
-                  <div className="animate__animated animate__fast">
-                        <h1>{show? "title1" : "title2"}</h1>
-                  </div>
-              </CSSTransition>
-          </SwitchTransition>
-          <button onClick={() => setShow(!show)}>按钮</button>
+      <div>
+          <div className="container">
+            <TransitionGroup appear component={null}>
+                {
+                    tasksList.map(t => (
+                        <CSSTransition timeout={2000} key={t.id}>
+                            <div>
+                                {t.name}
+                                <button onClick={() => {
+                                    var newTasks = tasksList.filter(it => it.id !== t.id);
+                                    setTasksList(newTasks)
+                                }}>删除</button>
+                            </div>
+                        </CSSTransition>
+                    ))
+                }
+            </TransitionGroup>
+        </div>
+        <button onClick={() => {
+            var name = window.prompt('请输入任务名称');
+            setTasksList([...tasksList,{id:name,name}])
+        }}>添加一个任务</button>
       </div>
     );
   };
