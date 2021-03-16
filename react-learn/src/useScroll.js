4cd0406716832d59696ed0307485f4a6a72ab60e
zhichengyuan <1798788388@qq.com>
2021-03-16 17:13:17 +0800

    yzc

---
+++ react-learn/src/useScroll.js
@@ -0,0 +1,8 @@
+import { useEffect } from 'react'
+import reset from './resetScroll'
+
+export default function useScroll(pathname) {
+    useEffect(() => {
+        reset();
+    },[pathname])
+}
---
+++ react-learn/src/withScroll.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import reset from './resetScroll'
+
+export default function withScroll(Comp) {
+    
+    return class ScrollWrapper extends React.Component {
+        componentDidMount(){
+            reset()
+        }
+        render() {
+            return <Comp {...this.props}/>
+        }
+    }
+}
