d253722d3fb23c7389e760b977ae67cdb871507c
zhichengyuan <1798788388@qq.com>
2021-03-18 16:52:21 +0800

    yzc

--- react-learn/src/redux/index.js
+++ react-learn/src/redux/index.js
@@ -4,7 +4,13 @@ import reducer from './reducer'
 
 const store = createStore(reducer);
 
-console.log(store.getState())
+
+console.log(store)
+
+//创建一个监听器
+const unSubscribe = store.subscribe(() => {
+    console.log('状态改变了',store.getState())
+})
 
 const boundUserAction = bindActionCreators(usersAction,store.dispatch);
 
@@ -16,11 +22,6 @@ boundUserAction.createAddUserAction({
     name:'用户3',
     age:23
 });//向仓库分发action
+unSubscribe();//取消监听
+boundUserAction.createDeleteUserAction(3);
 
-// store.dispatch(usersAction.createAddUserAction({
-//     id:4,
-//     name:'用户4',
-//     age:23
-// }))
-
-console.log(store.getState())
