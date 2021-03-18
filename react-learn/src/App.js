5fa9f799e523c23648da5784b26dad6c820dd730
zhichengyuan <1798788388@qq.com>
2021-03-18 11:56:31 +0800

    yzc

--- react-learn/src/App.js
+++ react-learn/src/App.js
@@ -1,27 +1,13 @@
 // import React,{useReducer} from  'react'
 import React from  'react'
-import { BrowserRouter as Router,Route,NavLink} from 'react-router-dom'
 import './App.css'
-import Page1 from './Page1';
-import Page2 from './Page2';
 
 
 
 class App extends React.Component {
     render() {
         return (
-            <Router getUserConfirmation={(msg,callback) => {
-                callback(window.confirm(msg))
-            }}>
-                <div className="nav">
-                    <NavLink to='/page1'>页面1</NavLink>
-                    <NavLink to='/page2'>页面2</NavLink>
-                </div>
-                <div className="container">
-                    <Route path="/page1" component={Page1}/>
-                    <Route path="/page2" component={Page2}/>
-                </div>
-            </Router>
+            <div><h1>reduex的管理数据</h1></div>
         )
     }
 }
