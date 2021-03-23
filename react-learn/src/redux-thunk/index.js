2833448de08ccd0629901ce070a8085cdee1e604
zhichengyuan <1798788388@qq.com>
2021-03-23 15:45:54 +0800

    yzc

--- react-learn/src/redux-thunk/index.js
+++ react-learn/src/redux-thunk/index.js
@@ -1,6 +1,16 @@
 function createThunkMiddleware(extra) {
     //该韩叔叔返回一个thunk中间件
     return store => next => action => {
-        action(store.dispatch,store.getState,extra);
+        if(typeof action === 'function') {
+            return action(store.dispatch,store.getState,extra)
+        }
+        else {
+            return next(action);
+        }
+        // action(store.dispatch,store.getState,extra);
     }
 }
+
+const thunk = createThunkMiddleware();
+thunk.withExtraArgument = createThunkMiddleware;
+export default thunk;
